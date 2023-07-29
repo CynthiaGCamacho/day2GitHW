12e5d6bfd568e51a952fc1bc712a781c7b3cf96b
cynthia <cynthia@LAPTOP-G4CRUIVD.fios-router.home>
2023-07-29 17:12:40 -0400

    Created HomeworkGit

---
+++ src/homework/HomeworkGit.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class HomeworkGit {
+	public static void main(String[]args) {
+		System.out.println("Cynthia Camacho...");
+		System.out.println("This is Git Homework");
+	}
+
+	public void add(int a, int b) {
+		System.out.println(a+b);
+	}
+}
